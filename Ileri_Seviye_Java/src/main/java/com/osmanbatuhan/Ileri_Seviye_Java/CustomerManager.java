18de249f54259e74e393ec64745d5200f086ff9e
Osman Batuhan Kalkan <osmankalkan21@istanbularel.edu.tr>
2025-03-23 20:11:00 +0300

    constructor-setter Injection

--- Ileri_Seviye_Java/src/main/java/com/osmanbatuhan/Ileri_Seviye_Java/CustomerManager.java
+++ Ileri_Seviye_Java/src/main/java/com/osmanbatuhan/Ileri_Seviye_Java/CustomerManager.java
@@ -1,12 +1,19 @@
 package com.osmanbatuhan.Ileri_Seviye_Java;
 
-public class CustomerManager {
-    private final ICustomerDal customerDal;
+public class CustomerManager implements ICustomerService{//interface ile beslemeliyiz.
+    /// Önemli Not:Eğer bir class herhangi bir implementasyon yada extends almazsa ileride bağımlılık problemlerine yaklanacağının işaretidir.
+    /// çıplak class kalmamalı.
+    private ICustomerDal customerDal;
 
-    public CustomerManager(ICustomerDal customerDal) {
-        this.customerDal = customerDal;
+    /// Constructor Injection
+    //public CustomerManager(ICustomerDal customerDal) {
+      //  this.customerDal = customerDal;
+    //}
+
+    /// setter Injection
+    public void setCustomerDal(ICustomerDal customerDal) {
+        this.customerDal=customerDal;
     }
-
     public void add(){
         //iş kuralları
         customerDal.add();
--- Ileri_Seviye_Java/src/main/java/com/osmanbatuhan/Ileri_Seviye_Java/Main.java
+++ Ileri_Seviye_Java/src/main/java/com/osmanbatuhan/Ileri_Seviye_Java/Main.java
@@ -6,14 +6,15 @@ public class Main {
     public static void main(String[] args) {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");//Xml yolunu gösterdik,bağladık
-
+        //CustomerManager manager = new CustomerManager(context.getBean("database",ICustomerDal.class));
         /// Arka planda bize newlenmiş halini getirir bizi 100lerce newlenmiş yapı yazmaktan kurtarır.
         /// Değişiklik yapmakisteidğimizde sadece class ismi değiştirmek yeterli örn:<bean id="database" class="com.osmanbatuhan.Ileri_Seviye_Java.CustomerDal">
         /// Birinde oracle veri tab. gelirken diğerinde myslq veri. tab. gelir.
     ///        <bean id="database" class="com.osmanbatuhan.Ileri_Seviye_Java.MysqlCustomerDal">
        ///         </bean>
 
-        CustomerManager manager = new CustomerManager(context.getBean("database",ICustomerDal.class));
-        manager.add();
+        ICustomerService customerService =context.getBean("service",ICustomerService.class);//Bu CustomerManager'in new'ini vermiş oldu
+        customerService.add();
+
     }
 }
